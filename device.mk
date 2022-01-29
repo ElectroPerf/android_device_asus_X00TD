b61fb81e822515f412ac70c754adafeae4a806c8
Kunmun <kunmun.devroms@gmail.com>
2022-01-29 21:59:37 +0530

    X00TD: Makefile: Drop height and width definitions
    • It was meant for bootanimation purposes and AOSPA doesn't require any such flags for bootanimation
    
    Change-Id: I4ec987358493f62061fecd233e32607abf4085d4

--- device.mk
+++ device.mk
@@ -7,10 +7,6 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2160
-TARGET_SCREEN_WIDTH := 1080
-
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
